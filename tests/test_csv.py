ff409fd09ad60e47ecce614e914b4ef352e3db78
Xamman27 <xamman27@gmail.com>
2023-06-28 23:46:50 +0400

    csv test in work

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,11 +1,15 @@
 import csv
+import os
+
+
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-with open('resources/eggs.csv', 'w') as csvfile:
+path_resources = os.path.join(os.path.dirname(os.path.dirname( os.path.abspath(__file__))), 'resources')
+with open(os.path.join(path_resources, 'eggs.csv'), 'x') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
     csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
     csvwriter.writerow(['Alex', 'Serj', 'Yana'])
 
-with open('resources/eggs.csv') as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        print(row)
+# with open('resources/eggs.csv') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         print(row)
