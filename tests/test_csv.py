39743a50a50dec822d449936b50c2a80c7d5eb0e
Xamman27 <xamman27@gmail.com>
2023-06-28 23:58:08 +0400

    csv test is done

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -3,13 +3,16 @@ import os
 
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-path_resources = os.path.join(os.path.dirname(os.path.dirname( os.path.abspath(__file__))), 'resources')
+path_resources = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'resources')
 with open(os.path.join(path_resources, 'eggs.csv'), 'x') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
     csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
     csvwriter.writerow(['Alex', 'Serj', 'Yana'])
+    assert os.path.isfile(os.path.join(path_resources, 'eggs.csv'))
 
-# with open('resources/eggs.csv') as csvfile:
-#     csvreader = csv.reader(csvfile)
-#     for row in csvreader:
-#         print(row)
+with open(os.path.join(path_resources, 'eggs.csv')) as csvfile:
+    csvreader = csv.reader(csvfile)
+    check_csv = []
+    for row in csvreader:
+        check_csv.append(row)
+    assert check_csv[0] == ['Anna', 'Pavel', 'Peter'] and check_csv[1] == ['Alex', 'Serj', 'Yana']
