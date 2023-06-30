7403f2d576e6b99135c36659cac9336870320f63
Xamman27 <xamman27@gmail.com>
2023-06-30 22:54:44 +0400

    test download with requests is done. Added assert and universal path.

--- tests/test_download_file_with_requests.py
+++ tests/test_download_file_with_requests.py
@@ -2,11 +2,12 @@ import os.path
 
 import requests
 # TODO оформить в тест, добавить ассерты, сохранять и читать из tmp, использовать универсальный путь
+tmp_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "tmp")
 url = 'https://selenium.dev/images/selenium_logo_square_green.png'
 
 response = requests.get(url)
-with open('selenium_logo.png', 'wb') as file:
+with open(os.path.join(tmp_path, 'selenium_logo.png'), 'wb') as file:
     file.write(response.content)
 
-size = os.path.getsize('selenium_logo.png')
-
+size = os.path.getsize(os.path.join(tmp_path, 'selenium_logo.png'))
+assert size == 30803
