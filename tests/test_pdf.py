6378e8f6569a9e5a22928476c6a9e4ae4c7e39eb
Xamman27 <xamman27@gmail.com>
2023-06-30 23:15:52 +0400

    Test pdf is done

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,9 +1,16 @@
 from pypdf import PdfReader
+import os
+
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-reader = PdfReader("../resources/docs-pytest-org-en-latest.pdf")
+path_to_pdf = os.path.join(os.path.dirname(os.path.dirname(__file__)), "resources")
+reader = PdfReader(os.path.join(path_to_pdf, 'docs-pytest-org-en-latest.pdf'))
 number_of_pages = len(reader.pages)
 page = reader.pages[0]
 text = page.extract_text()
 print(page)
 print(number_of_pages)
 print(text)
+
+assert number_of_pages == 412
+assert text == "pytest Documentation\nRelease 0.1\nholger krekel, trainer and \
+consultant, https://merlinux.eu/\nJul 14, 2022"
